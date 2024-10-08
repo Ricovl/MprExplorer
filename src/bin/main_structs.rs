c3536a8f405e2ecc6a48f48c3bbef1d6550b3dac
Ricovl <18077338+Ricovl@users.noreply.github.com>
2024-10-08 23:18:41 +0200

    Fix uuid formatting on json project export

--- src/bin/main_structs.rs
+++ src/bin/main_structs.rs
@@ -2,7 +2,8 @@ use bson::Document;
 use rayon::iter::{IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator};
 use rusqlite::Connection;
 use serde::Serialize;
-use std::{collections::{HashMap, HashSet}, fs::{self, OpenOptions}, io::{Seek, Write}, path::Path, str::FromStr};
+use serde_json::Value;
+use std::{fs::{self}, io::Write, path::Path, str::FromStr};
 use uuid::Uuid;
 
 #[derive(Debug, PartialEq, Clone, Serialize)]
@@ -61,12 +62,12 @@ impl Unit {
         contents: &Vec<u8>,
     ) -> Unit {
         Unit {
-            unit_id: unit_id,
-            container_id: container_id,
+            unit_id,
+            container_id,
             containment_name: UnitType::from_str(containment_name.as_str()).unwrap(),
-            tree_conflicts: tree_conflicts,
-            contents_hash: contents_hash,
-            contents_conflicts: contents_conflicts,
+            tree_conflicts,
+            contents_hash,
+            contents_conflicts,
             contents: contents.to_vec(),
             // doc: Document::from_reader(contents.as_slice()).unwrap(),
             doc: None,
@@ -101,36 +102,48 @@ fn get_all_units(conn: &Connection) -> Result<Vec<Unit>, &str> {
     Ok(units)
 }
 
-
-fn bson_to_string(bson: &bson::Bson) -> &'static str {
-    match bson {
-        bson::Bson::Double(_) => "Double",
-        bson::Bson::String(_) => "String",
-        bson::Bson::Boolean(_) => "bool",
-        bson::Bson::Null => "Null",
-        bson::Bson::RegularExpression(_) => "RegEx",
-        bson::Bson::JavaScriptCode(_) => "JavaScript",
-        bson::Bson::JavaScriptCodeWithScope(_) => "JavaScriptScope",
-        bson::Bson::Int32(_) => "i32",
-        bson::Bson::Int64(_) => "i64",
-        bson::Bson::Timestamp(_) => "Timestamp",
-        bson::Bson::Binary(_) => "Binary",
-        bson::Bson::ObjectId(_) => "ObjectId",
-        bson::Bson::DateTime(_) => "DateTime",
-        bson::Bson::Symbol(_) => "Symbol",
-        bson::Bson::Decimal128(_) => "Decimal128",
-        bson::Bson::Undefined => "Undefined",
-        bson::Bson::MaxKey => "MinKey",
-        bson::Bson::MinKey => "MaxKey",
-        bson::Bson::DbPointer(_) => "DbPointer",
-        _ => "unknown",
+fn process_uuids(value: &mut Value) {
+    match value {
+        Value::Object(map) => {
+            for val in map.values_mut() {
+                process_uuids(val);
+            }
+        }
+        Value::Array(arr) => {
+            if arr.len() == 16 && arr.iter().all(|n| n.is_u64()) {
+                let bytes: Vec<u8> = arr
+                    .iter()
+                    .filter_map(|n| n.as_u64().and_then(|n| {
+                        if n <= 255 {
+                            Some(n as u8)
+                        } else {
+                            None
+                        }
+                    }))
+                    .collect();
+                if bytes.len() == 16 {
+                    if let Ok(uuid) = Uuid::from_slice(&bytes) {
+                        *value = Value::String(uuid.to_string());
+                    }
+                }
+            } else {
+                for item in arr.iter_mut() {
+                    process_uuids(item);
+                }
+            }
+        }
+        _ => {}
     }
 }
-
 
 fn generate_structure(units: &[Unit], root_folder: &str) -> std::io::Result<()> {
     let root_unit = units.iter().find(|u| u.unit_id == u.container_id).expect("Root unit not found");
 
+    let root_folder = Path::new(root_folder);
+
+    if root_folder.exists() {
+        fs::remove_dir_all(&root_folder)?;
+    }
     fs::create_dir_all(root_folder)?;
 
     // Recursive function to create structure
@@ -162,27 +175,52 @@ fn generate_structure(units: &[Unit], root_folder: &str) -> std::io::Result<()> 
                 UnitType::Folders | UnitType::Modules => {
                 },
                 _ => {
-                    let file_name = format!("{:?}_{}_{}.json", unit.containment_name, name, unit.unit_id);
+                    let file_name = match unit.containment_name {
+                        UnitType::Documents => {
+                            format!("{}.json", name)
+                        },
+                        UnitType::DomainModel | UnitType::ModuleSettings | UnitType::ModuleSecurity => {
+                            format!("{:?}.json", unit.containment_name)
+                        },
+                        _ => {
+                            format!("{:?}_{}.json", unit.containment_name, unit.unit_id)
+                        }
+                    };
                     let file_path = current_path.join(file_name);
                     let mut file = fs::File::create(&file_path)?;
-                    // writeln!(file, "Unit ID: {}\nContainer ID: {}\nType: {:?}\n", 
-                    // unit.unit_id, unit.container_id, unit.containment_name)?;
 
-                    // if let Ok(pretty_json) = serde_json::to_string_pretty(doc) {
-                    //     writeln!(file, "{}", pretty_json)?;
-                    // }
-                    // serde_json::to_writer_pretty(&mut file, doc)?;
-                    let json = serde_json::to_string_pretty(unit)?;
+                    let mut value = serde_json::to_value(&unit)?;
+                    process_uuids(&mut value);
+                    let json = serde_json::to_string_pretty(&value)?;
                     file.write_all(json.as_bytes())?;
                 }
             };
             
         }
 
-        Ok(())
+        Ok::<(), std::io::Error>(())
     }
 
     create_structure(root_unit, units, Path::new(root_folder))?;
+
+    Ok(())
+}
+
+
+fn generate_structurev2(units: &[Unit], root_folder: &str) -> std::io::Result<()> {
+
+    fs::create_dir_all(root_folder)?;
+
+    units.par_iter().try_for_each(|unit| {
+        let first_folder = &unit.unit_id.to_string()[0..2];
+        let second_folder = &unit.unit_id.to_string()[2..4];
+        println!("{}/{}/{}.mxunit", first_folder, second_folder, unit.unit_id);
+
+        fs::create_dir_all(format!("{}/{}/{}/", root_folder, first_folder, second_folder))?;
+        let mut file = fs::File::create(format!("{}/{}/{}/{}.mxunit", root_folder, first_folder, second_folder, unit.unit_id))?;
+        file.write_all(&unit.contents)?;
+        Ok::<(), std::io::Error>(())
+    })?;
 
     Ok(())
 }
@@ -195,7 +233,9 @@ fn main() {
     let units = get_all_units(&conn).unwrap();
     println!("Finished mpr read.");
 
-    let root_folder = "/home/rico/MX";
+    // let root_folder = "resources/mprcontents";
+    // generate_structurev2(&units, root_folder).unwrap();
+    let root_folder = "/home/rico/MX/src";
     generate_structure(&units, root_folder).unwrap();
 
     println!("Folder structure generated successfully!");
